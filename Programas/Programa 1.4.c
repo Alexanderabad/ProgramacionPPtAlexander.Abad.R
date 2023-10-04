41560a34da8d0d9c4d74721223856c05d569fe1d
Alexander <Alexanderabadr@gmail.com>
2023-10-04 17:57:53 -0400

    mi primer commit

---
+++ Programas/Programa 1.4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* Superficie del Triangulo.
+El Programa, al recibir como datos la base y la altura de un triangulo,
+Calcula su superficie.
+
+BAS, ALT y SUP: variables de tipo real. */
+
+void main (void)
+{
+    float BAS, ALT, SUP;
+    printf("Ingrese la base y la altura del triangulo: ");
+    scanf("%f %f", &BAS, &ALT);
+    SUP = BAS * ALT / 2;
+    printf("\nLa superfcie del triangulo es: %5.2f", SUP);
+}
---
+++ Programas/for1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i = 1000;
+
+    for (i = 100;i--)
+    {i
+    */
+
+    i = 100;
+    do
+    {
+
+    printf
+    }
+
+}
