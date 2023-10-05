c054f0867460cb299f1ae9a25f93f850791c9ff0
Alexander <144921839+Alexanderabad@users.noreply.github.com>
2023-10-04 21:10:25 -0400

    commit 4

---
+++ Programas/programa 2.3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/* Promedio
+El programa, al recibir como dato el promedio de un alumno en un curso
+universitario, escrcibe aprobado si su promedio es mayor a 7, o
+reprobado en caso contrario.
+
+PRO: variable de tipo real. */
+
+void main(void)
+{
+    float PRO;
+    Printf("Ingrese el promedio del alumno: ");
+    scanf("%f", &PRO);
+    if PRO >= 6.0)
+    printf("\nReprobado");
+    else
+        printf("Aprobado");
+}
