865724f72fc5b5e0ceeda6c1e7b83effe406d291
Alexander <144921839+Alexanderabad@users.noreply.github.com>
2023-10-04 20:25:45 -0400

    tercer commit

--- Programas/Programa 1.4.c
+++ Programas/Programa 1.4.c
@@ -10,7 +10,7 @@ void main (void)
 {
     float BAS, ALT, SUP;
     printf("Ingrese la base y la altura del triangulo: ");
-    scanf("%f %f", &BAS, &ALT);
+    scanf("%f  %f", &BAS, &ALT);
     SUP = BAS * ALT / 2;
     printf("\nLa superfcie del triangulo es: %5.2f", SUP);
 }
